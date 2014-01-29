6be1649cbd9b15f72f8e08c2f45dc3f6a0ce48df
Tim Park <timfpark@gmail.com>
2014-01-28 22:06:38 -0800

    add defaults for service config parameters

--- n2.js
+++ n2.js
@@ -4,4 +4,10 @@ var cli = require('./lib')
 var options = optimist.argv;
 var commands = optimist.argv._;
 
-cli.execute(commands, options, function() {});
+cli.load(function(err) {
+	if (err) return console.log(err);
+
+	cli.execute(commands, options, function(err) {
+		if (err) return console.log(err);
+	});
+});
