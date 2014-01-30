94419cbd4ff4996c085568a6454f64d64b12467f
Tim Park <timfpark@gmail.com>
2014-01-30 11:03:36 -0800

    implement user login and seperate store for config values

---
+++ test/units/service.js
@@ -0,0 +1,25 @@
+﻿var assert = require('assert'),
+    service = require('../../lib/service'),
+    store = require('../../lib/store');
+
+describe('service', function() {
+
+    it('should be able to configure a value and get an updated config.', function(done) {
+        service.execute(['config', 'protocol','http'], {}, function(err) {
+	        assert.ifError(err);
+
+            store.get('protocol', function(err, value) {
+                assert.ifError(err);
+                assert.equal(value, 'http');
+
+                service.getConfig(function(err, config) {
+                    assert.ifError(err);
+
+                    assert.equal(config['protocol'], 'http');
+                    done();
+                });
+            });
+    	});
+    });
+
+});
