622b6a7318b41407877d8d4aec7e30018d451ea4
Tim Park <timfpark@gmail.com>
2014-01-31 13:18:46 -0800

    wip on refactor

--- test/units/service.js
+++ test/units/service.js
@@ -1,18 +1,18 @@
 ﻿var assert = require('assert'),
-    service = require('../../lib/service'),
-    store = require('../../lib/store');
+    cli = require('../../lib');
 
 describe('service', function() {
 
     it('should be able to configure a value and get an updated config.', function(done) {
-        service.execute(['config', 'protocol','http'], {}, function(err) {
+        cli.arguments = ['config', 'protocol','http'];
+        cli.service.execute(function(err) {
 	        assert.ifError(err);
 
-            store.get('protocol', function(err, value) {
+            cli.store.get('protocol', function(err, value) {
                 assert.ifError(err);
                 assert.equal(value, 'http');
 
-                service.getConfig(function(err, config) {
+                cli.service.getConfig(function(err, config) {
                     assert.ifError(err);
 
                     assert.equal(config['protocol'], 'http');
