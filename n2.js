a8a65d39d4d7fb0fe300aa9718f8461be1d04f76
Tim Park <timfpark@gmail.com>
2014-01-31 19:59:06 -0800

    exit after successful command execution

--- n2.js
+++ n2.js
@@ -6,5 +6,7 @@ var commandArguments = optimist.argv._;
 cli.arguments = optimist.argv._;
 
 cli.execute(function(err) {
-	if (err) return console.log('command failed: ' + err);
+    if (err) return console.log('command failed: ' + err);
+
+    process.exit(0);
 });
