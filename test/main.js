d6a45d1f0c9369385467d25adfff255ed7088247
Ivan R. Judson <irjudson@gmail.com>
2014-11-11 09:32:58 -0800

    Tests implemented, service tests fail with > 1 test

--- test/main.js
+++
@@ -1,9 +0,0 @@
-var store = require('../lib/store');
-
-before(function(done) {
-    store.clear(done);
-});
-
-after(function(done) {
-    store.clear(done);
-});
--- test/units/service.js
+++ test/units/service.js
@@ -3,23 +3,25 @@
 
 describe('service', function() {
 
+    it('should be able to show the current configuration.', function(done) {
+        cli.currentArgument = 0;
+        cli.arguments = [ 'show' ];
+        cli.service.execute(function(err) {
+            assert.ifError(err);
+            done();
+        });
+    });
+
     it('should be able to configure a value and get an updated config.', function(done) {
-        cli.arguments = ['set', 'protocol','http'];
+        cli.currentArgument = 0;
+        cli.arguments = [ 'set', 'protocol', 'http' ];
         cli.service.execute(function(err) {
 	        assert.ifError(err);
-
-            cli.store.get('protocol', function(err, value) {
+            cli.service.getConfig(function(err, config, service) {
                 assert.ifError(err);
-                assert.equal(value, 'http');
-
-                cli.store.getAll(function(err, config) {
-                    assert.ifError(err);
-
-                    assert.equal(config['protocol'], 'http');
-                    done();
-                });
+                assert.equal(config['protocol'], 'http');
+                done();
             });
     	});
     });
-
 });
